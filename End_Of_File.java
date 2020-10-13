bfd79b320e6e9e659db8c2dbcd678a9df4390d19
karadagserhat <71098921+karadagserhat@users.noreply.github.com>
2020-10-13 11:30:18 +0300

    Add files via upload

---
+++ End_Of_File.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class End_Of_File {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int i=0;
+		while(sc.hasNext()) {
+			i++;
+			System.out.println(i+""+sc.nextLine());
+		}
+		sc.close();
+	}
+}
