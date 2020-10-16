f0626d6caa3ac68402a51a8a7b649608533c5b51
karadagserhat <71098921+karadagserhat@users.noreply.github.com>
2020-10-16 10:20:33 +0300

    Add files via upload

---
+++ SetHash.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+
+public class SetHash {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+//Write your code here
+  HashSet<String> pairs = new HashSet<String>(t);
+
+    for(int i = 0; i < t; i++)
+    {
+        //pairs.add("(" + pair_left[i] + ", " + pair_right[i] + ")" );
+        //pairs.add(Arrays.asList(pair_left[i],pair_right[i]));
+         pairs.add(pair_left[i] + " " + pair_right[i]);
+        System.out.println(pairs.size());        
+    }
+    s.close();
+    }
+}
